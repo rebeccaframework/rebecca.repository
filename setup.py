cba7ae11d8cc71211e9431e241048a432be483e9
Atsushi Odagiri <aodagx@gmail.com>
2013-08-24 17:31:42 +0900

    prep for 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.0'
+version = '0.1.1'
 
 requires = [
     "setuptools",
@@ -31,7 +31,7 @@ long_description = (
 
 setup(name='rebecca.repository',
       version=version,
-      description="",
+      description="helper utility for repository pattern of PofEAA",
       long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
