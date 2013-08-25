8420a402946851280c353160042fd348b9b9c796
Atsushi Odagiri <aodagx@gmail.com>
2013-08-25 17:18:29 +0900

    added venusian decorator

--- src/rebecca/repository/__init__.py
+++ src/rebecca/repository/__init__.py
@@ -1,1 +1,8 @@
+from .config import includeme
+from .interfaces import IRepository
+from .decorators import repository_config
 
+
+def get_repository(request, name):
+    reg = request.registry
+    return reg.queryUtility(IRepository, name=name)
--- tests/test_it.py
+++ tests/test_it.py
@@ -1,4 +1,24 @@
-import rebecca.repository
+from pyramid import testing
+from pyramid.config import Configurator
+from rebecca.repository import repository_config
+from rebecca.repository import get_repository
 
-def test_it():
-    pass
+
+def test_app():
+    conf = Configurator()
+    conf.include('rebecca.repository')
+    conf.scan(".")
+    conf.commit()
+    request = testing.DummyResource(registry=conf.registry)
+    result = get_repository(request, "testing")
+
+    assert isinstance(result, TestingRepository)
+    assert result.value == 9999
+
+@repository_config(name="testing", args=(9999,))
+class TestingRepository(object):
+    """ testing
+    """
+
+    def __init__(self, value):
+        self.value = value
